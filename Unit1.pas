29eebf80c9db2d7894ca4565e7e0e8f5fd6e0cab
Carlos Baldoino <carlosbaldoino2005@hotmail.com>
2023-03-07 22:45:34 -0300

    add

--- Unit1.pas
+++ Unit1.pas
@@ -5,10 +5,13 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.FMTBcd, Data.DB, Data.SqlExpr,
-  Vcl.StdCtrls, FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Param,
-  FireDAC.Stan.Error, FireDAC.DatS, FireDAC.Phys.Intf, FireDAC.DApt.Intf,
-  FireDAC.Stan.Async, FireDAC.DApt, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
-  Horse,Horse.Jhonson,Horse.Commons, System.JSON;
+  Vcl.StdCtrls,
+  Horse,Horse.Jhonson,Horse.Commons, Horse.BasicAuthentication,
+  Horse.Compression,Horse.HandleException, Horse.OctetStream,
+  Horse.Logger, // It's necessary to use the unit
+  Horse.Logger.Provider.LogFile, // It's necessary to use the unit
+  Horse.Paginate,Horse.Etag,
+  System.JSON;
 
 type
   TForm1 = class(TForm)
@@ -22,11 +25,13 @@ type
     Caminho : String;
     Users: TJsonArray;
     User : TJsonObject;
+    LLogFileConfig: THorseLoggerLogFileConfig;
   public
     { Public declarations }
     procedure GetUsuario(Req: THorseRequest; Res: THorseResponse; Next: TProc);
     procedure SetUsuario(Req: THorseRequest; Res: THorseResponse; Next: TProc);
     procedure DeleteUsuario(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+    function Autencicacao(const Ausername, APassWord: String):Boolean;
     procedure Iniciar;
   end;
 
@@ -38,6 +43,11 @@ implementation
 {$R *.dfm}
 
 uses Dataset.Serialize;
+
+function TForm1.Autencicacao(const Ausername, APassWord: String): Boolean;
+begin
+  Result := Ausername.Equals('user') and APassWord.Equals('password');
+end;
 
 procedure TForm1.Button1Click(Sender: TObject);
 begin
@@ -58,7 +68,19 @@ begin
 
   if not App.IsRunning then
   begin
-    App.Use(Jhonson);
+
+    LLogFileConfig := THorseLoggerLogFileConfig.New
+    .SetLogFormat('${request_clientip} [${time}] ${response_status}');
+
+    THorseLoggerManager.RegisterProvider(THorseLoggerProviderLogFile.New(LLogFileConfig)); // Log
+    App.Use(Compression());  // Comprimir
+    App.Use(Jhonson);    // Tipo de requisição
+    App.Use(HandleException);  // Exception
+    App.Use(HorseBasicAuthentication(Autencicacao)); // Autenticação
+    App.Use(OctetStream);  // Trafegar arquivo, imagem
+    App.Use(Paginate);
+    App.Use(THorseLoggerManager.HorseCallback);
+    App.Use(eTag); //https://github.com/academiadocodigo/Horse-ETag
     App.Get(Caminho,GetUsuario);
     App.Post(Caminho,SetUsuario);
     App.Delete(Caminho+'/:id',DeleteUsuario);
@@ -77,10 +99,14 @@ begin
 end;
 
 procedure TForm1.SetUsuario(Req: THorseRequest; Res: THorseResponse; Next: TProc);
+var i : Integer;
 begin
+  for I := 0 to 10000 do begin
+
   User := Req.Body<TJsonObject>.Clone as TJsonObject;
   Users.AddElement(User);
   Res.Send<TJsonAncestor>(User.Clone).Status(THttpStatus.Created);
+  end;
 end;
 
 procedure TForm1.DeleteUsuario(Req: THorseRequest; Res: THorseResponse; Next: TProc);
